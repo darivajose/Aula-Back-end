4226bfecde3b7fb211ad9c3f84304f9a7aff5b7a
josedariva <167941121+darivajose@users.noreply.github.com>
2026-05-08 07:55:03 -0300

    operadores arimeticos

---
+++ operadores_logicos/Aritimeticos.js
@@ -0,0 +1,28 @@
+// operadores aritméticos
+// + adição
+// - subtração
+// * multiplicação
+// / divisão
+// % módulo (resto da divisão)
+// ** exponenciação
+
+let numero1 = 10
+let numero2 = 5
+ 
+console.log("Adição: " + (numero1 + numero2)) // 15
+console.log("Subtração: " + (numero1 - numero2)) // 5
+console.log("Multiplicação: " + (numero1 * numero2)) // 50
+console.log("Divisão: " + (numero1 / numero2)) // 2
+console.log("Módulo: " + (numero1 % numero2)) // 0
+console.log("Exponenciação: " + (numero1 ** numero2)) // 100000
+
+// Exemplo: Verificar se um número é par ou ímpar usando o operador módulo
+//Sintaxe: dividendo % divisor
+/*
+let num = 7;
+if (num % 2 === 0) {
+    console.log("Par");
+} else {
+    console.log("Ímpar");
+}
+*/

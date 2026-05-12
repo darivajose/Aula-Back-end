6753823b7a0fb6d746d8f0a318ea0fc521fd61af
josedariva <167941121+darivajose@users.noreply.github.com>
2026-05-12 17:48:46 -0300

    novas funcionalidades

--- Laço_De_Repetição/for.js
+++ Laço_De_Repetição/for.js
@@ -6,3 +6,10 @@ for (let contador = 0; contador < 8; contador+=2){
     console.log(contador)
     console.log("aumentar o contador")
 }
+
+let pontoDeVida = 0 
+
+for(let i = 0; i < 11; i++){
+    pontosDeVida += 1
+    //pontosDeVida = pontosDevidas + 1
+}

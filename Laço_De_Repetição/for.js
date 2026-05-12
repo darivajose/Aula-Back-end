645ea3304d1f2305a104d4897feec96769794244
josedariva <167941121+darivajose@users.noreply.github.com>
2026-05-12 17:59:30 -0300

    mais for

--- Laço_De_Repetição/for.js
+++ Laço_De_Repetição/for.js
@@ -1,15 +1,17 @@
 /*
 
 */
-
+/*
 for (let contador = 0; contador < 8; contador+=2){
     console.log(contador)
     console.log("aumentar o contador")
 }
-
+*/
 let pontoDeVida = 0 
 
-for(let i = 0; i < 11; i++){
-    pontosDeVida += 1
-    //pontosDeVida = pontosDevidas + 1
+for(let i = 0; i <= 11; i++){
+    pontoDeVida += 1
+    //pontoDeVida = pontoDevidas + 1
+    console.log("Tomou poção mágica" +1)
 }
+console.log(pontoDeVida + "Totais")
